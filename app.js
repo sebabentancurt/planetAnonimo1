1cd41d885e1e932467ea8359978412ad29734cd8
Sebastian Bentancurt <sebabentancurt@gmail.com>
2013-03-29 02:59:37 -0300

    F

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path')
-  ,mongoose = require('mongoose');
+  , mongoose = require('mongoose');
 
 var app = express();
 
+
+
 //Conecto a MongoDB con connect
-mongoose.connect('mongodb://localhost/planetAnonimo');
+mongoose.connect('mongodb://sebabentancurt:seba5641456414@ds035237.mongolab.com:35237/af_planetanonimo-sebabentancurt'); 
 
 // Configuracion
 app.configure(function(){
@@ -25,6 +27,11 @@ app.configure(function(){
   app.use(express.methodOverride());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
+  
+  //Cookies session
+   app.use(express.bodyParser());
+  app.use(express.cookieParser('nhispano'));
+  app.use(express.session({secret: 'SECRET' }));
 });
 
 app.configure('development', function(){
@@ -36,12 +43,53 @@ app.get('/', routes.index);
 app.get('/users', user.list);
 app.get('/pagina', routes.pagina);
 
-
-
 //Ejecutar servidor express
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
+
 });
 
-//Obtencion datos POST
+var schema = mongoose.Schema({  name: 'string'
+                               ,pass: 'string'
+                               ,email: 'string'
+                               ,username: 'string'
+                               ,fecha: {nacimiento:'date', altas:'array', bajas:'array'}
+                               ,localidad: {ciudad: 'string', pais: 'string', paisGeo:'string'}
+                               ,activo: 'boolean'
+                               ,pregunta: 'string'
+                               ,comentario: 'string'
+                               ,slogan:'string'
+                               ,foto: 'string'
+                               ,genero: 'string'
+                               ,faceLogin: 'array'
+                               ,twitLogin: 'array'
+                               ,faceRegistro: 'boolean'
+                               ,twitRegistro: 'boolean'
+                               });
+var usuarios = mongoose.model('usuarios', schema);
 
+//Obtencion datos POST
+app.post('/inicio', function(request, response){
+var user = new usuarios({ name: ''
+                         ,pass: request.body.registroPass
+                         ,email: request.body.registroEmail
+                         ,username: request.body.registroUser
+                         ,fecha: {nacimiento:null ,altas: new Date(Date.now()) , bajas: null}
+                         ,localidad: {ciudad: null, pais: null, paisGeo:null}
+                         ,activo: true
+                         ,pregunta: null
+                         ,comentario: null
+                         ,slogan: null
+                         ,foto: null
+                         ,genero: null
+                         ,faceLogin: 'no'
+                         ,twitLogin: 'no'
+                         ,faceRegistro: false
+                         ,twitRegistro: false
+                         });
+user.save(function (err) {
+  if (err) // ...
+  console.log('meow');
+});
+
+});
